01d3d2bfaa2c7ba1866ab97bb7c53c36c2c3c107
Suguru Tokuda <suguru.tokuda@gmail.com>
2026-04-28 14:24:58 -0700

    Initial Commit

---
+++ PoochyLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoochyLog
+//
+//  Created by Suguru Tokuda on 4/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
